a67e039aeedccf6136130d4fad9cc4d31fa69783
JKCoCo <1226848216@qq.com>
2019-01-08 15:14:31 +0800

    WaveView

---
+++ app/src/main/java/xyz/rao77/waveview/MainActivity.java
@@ -0,0 +1,13 @@
+package xyz.rao77.waveview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
